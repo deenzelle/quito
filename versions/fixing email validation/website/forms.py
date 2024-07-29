dd0e4a2eb9ddd9054155496515f6832c72842c0b
deenzelle <126622294+deenzelle@users.noreply.github.com>
2024-07-29 21:35:41 +1200

    fixing email validation
    changed it so that there are actual validators in place to ensure that no random string of text could be classified as an email

--- versions/fixing email validation/website/forms.py
+++ versions/fixing email validation/website/forms.py
@@ -4,26 +4,19 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextA
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from .models import User
 
-
-
-
-
-
-
-
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators = [DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators = [DataRequired(), Email()])
     password = PasswordField('Password', validators = [DataRequired()])
-    confirm_password = PasswordField('Password', validators = [DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
     
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data)
+        user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is taken, please choose a different one')
         
     def validate_email(self, email):
-        user = User.query.filter_by(email=email.data)
+        user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is taken, please choose a different one')
