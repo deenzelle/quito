676e0b54cd2318ea813b95d4434cfad91e3b56f1
deenzelle <126622294+deenzelle@users.noreply.github.com>
2024-08-08 10:31:25 +1200

    pagination fixed in new version

--- versions/making a title field for posts (4)/website/views.py
+++ versions/making a title field for posts (4)/website/views.py
@@ -18,7 +18,9 @@ def home():
 @views.route("/blog")
 @login_required
 def blog():
-    posts = Post.query.all()
+    #posts = Post.query.all()
+    page = request.args.get('page', 1, type = int)
+    posts = Post.query.order_by(Post.date_created.desc()).paginate(page=page, per_page = 4)
     return render_template("blog.html", user=current_user, posts=posts)
 
 @views.route("/create-post", methods=['GET', 'POST'])
